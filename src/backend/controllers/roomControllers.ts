5ab4f3b43074677c5a62ad96bac04edaa9523cdb
sahil70180 <sahilkumarhp20@gmail.com>
2024-10-26 16:27:53 +0530

    Handle Create Room and fetch all rooms from DB

--- src/backend/controllers/roomControllers.ts
+++ src/backend/controllers/roomControllers.ts
@@ -1,7 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
+import Room from "../models/room";
 
 export const allRooms = async (req: NextRequest) => {
+  const data = await Room.find();
+
   return NextResponse.json({
-    data: "Test from Controllers (All Rooms)",
+    success: "true",
+    message: "All Rooms fetched Successfully",
+    data,
+  });
+};
+
+export const newRoom = async (req: NextRequest) => {
+  const body = await req.json();
+  const room = await Room.create(body);
+
+  return NextResponse.json({
+    success: true,
+    message: "Room Created Successfully",
+    room,
   });
 };
