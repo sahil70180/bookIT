882b9716c8c86deee9dc218dedc26dff4d058c16
sahil70180 <sahilkumarhp20@gmail.com>
2024-11-18 16:56:25 +0530

    Fecth data in server component Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import Home from "@/components/Home";
 import Image from "next/image";
 
-export default function HomePage() {
+const getAllRooms = async () => {
+  const res = await fetch(`${process.env.localURI}/api/rooms`);
+  return res.json();
+};
+
+export default async function HomePage() {
+  const data = await getAllRooms();
+  console.log("===============");
+  console.log("Data : ", data.resPerPage);
+  console.log("================");
   return (
     <div className="container">
       <Home />
